569129d29c4bd42ff93bde0c2db67ae4d262ff28
apolloparty <apolloparty@gmail.com>
2019-02-10 22:22:16 +0100

    trying to paste on image with pygame

--- labyrinth.py
+++ labyrinth.py
@@ -31,7 +31,7 @@ class Input_user:
                         move = "q"
                         return move
                 if event.type == QUIT:
-                    passing = 0
+                    quit()
 
 class Characters:
 
@@ -68,62 +68,85 @@ class Motion:
                 maps[position] = " "
                 maps = "".join(maps)
                 print(maps)
+                directionX = 0
+                directionY = -32
             elif tina == True and maps[position - 16] == "T":
                 maps[position - 16] = "M"
                 maps[position] = " "
                 maps = "".join(maps)
                 print(maps)
                 print("YOU WIN")
+                directionX = 0
+                directionY -32
             else:
                 maps = "".join(maps)
                 print(maps)
+                directionX = 0
+                directionY = 0
         if move == "S" or move == "s":
             if maps[position + 16] == " " or maps[position + 16] == "X" or maps[position + 16] == "Y" or maps[position + 16] == "Z":
                 maps[position + 16] = "M"
                 maps[position] = " "
                 maps = "".join(maps)
                 print(maps)
+                directionX = 0
+                directionY = 32
             elif tina == True and maps[position + 16] == "T":
                 maps[position + 16] = "M"
                 maps[position] = " "
                 maps = "".join(maps)
                 print(maps)
+                directionX = 0
+                directionY = 32
                 print("YOU WIN")
             else:
                 maps = "".join(maps)
                 print(maps)
-                direction = (0, 0)
+                directionX = 0
+                directionY = 0
         if move == "Q" or move == "q":
             if maps[position - 1] == " " or maps[position - 1] == "X" or maps[position - 1] == "Y" or maps[position - 1] == "Z":
                 maps[position - 1] = "M"
                 maps[position] = " "
                 maps = "".join(maps)
                 print(maps)
+                directionX = -32
+                directionY = 0
             elif tina == True and maps[position - 1] == "T":
                 maps[position - 1] = "M"
                 maps[position] = " "
                 maps = "".join(maps)
                 print(maps)
+                directionX = -32
+                directionY = 0
                 print("YOU WIN")
             else:
                 maps = "".join(maps)
                 print(maps)
+                directionX = 0
+                directionY = 0
         if move == "D" or move == "d":
             if maps[position + 1] == " " or maps[position + 1] == "X" or maps[position + 1] == "Y" or maps[position + 1] == "Z":
                 maps[position + 1] = "M"
                 maps[position] = " "
                 maps = "".join(maps)
                 print(maps)
+                directionX = 32
+                directionY = 0
             elif tina == True and maps[position + 1] == "T":
                 maps[position + 1] = "M"
                 maps[position] = " "
                 maps = "".join(maps)
                 print(maps)
+                directionX = -32
+                directionY = 0
                 print("YOU WIN")
             else:
                 maps = "".join(maps)
                 print(maps)
-        return maps
+                directionX = 0
+                directionY = 0
+        return directionX, directionY
         
 class Items:
 
@@ -247,9 +270,8 @@ class Calc:
 
 class Pygame:
 
-    def __init__(self, maps, maccoord, tinacoord, Xcoord, Ycoord, Zcoord):
+    def __init__(self, maps, tinacoord, Xcoord, Ycoord, Zcoord):
         self.maps = maps
-        self.maccoord = maccoord
         self.tinacoord = tinacoord
         self.Xcoord = Xcoord
         self.Ycoord = Ycoord
@@ -267,6 +289,16 @@ class Pygame:
         position_space = space.get_rect()
         wall = pygame.image.load("ressource/mur.png").convert()
         position_wall = wall.get_rect()
+        mac = pygame.image.load("ressource/MacGyver.png").convert_alpha()
+        position_mac = mac.get_rect()
+        itemX = pygame.image.load("ressource/item1.png").convert_alpha()
+        disX = itemX.get_rect()
+        itemY = pygame.image.load("ressource/item2.png").convert_alpha()
+        disY = itemY.get_rect()
+        itemZ = pygame.image.load("ressource/item3.png").convert_alpha()
+        disZ = itemZ.get_rect()
+        itemtina = pygame.image.load("ressource/Gardien.png").convert_alpha()
+        disTina = itemtina.get_rect()
         while i != 239:
             if maps[i] == " ":
                 window.blit(space, ((i - (16 * j)) * 32, j * 32))
@@ -274,41 +306,71 @@ class Pygame:
                 i = i + 1
             elif maps[i] == "#":
                 window.blit(wall, ((i - (16 * j)) * 32, j * 32))
-                #pygame.display.flip()
+                pygame.display.flip()
                 i = i + 1
             elif maps[i] == "\n":
                 j = j + 1
                 i = i + 1
             else:
                 i = i + 1
-        return window, space
+        return window, space, mac, itemX, itemY, itemZ, itemtina, position_mac
 
-    def item_display(self, window):
+    def item_display(self, window, itemX, itemY, itemZ, itemtina):
         Xcoord = self.Xcoord
         Ycoord = self.Ycoord
         Zcoord = self.Zcoord
+        tinacoord = self.tinacoord
 
-        itemX = pygame.image.load("ressource/item1.png").convert_alpha()
-        disX = itemX.get_rect()
         window.blit(itemX, Xcoord)
-        itemY = pygame.image.load("ressource/item2.png").convert_alpha()
-        disY = itemY.get_rect()
         window.blit(itemY, Ycoord)
-        itemZ = pygame.image.load("ressource/item3.png").convert_alpha()
-        disZ = itemZ.get_rect()
         window.blit(itemZ, Zcoord)
-        pygame.display.flip()
-    
-    def mac_display(self, window, space):
+        window.blit(itemtina, tinacoord)
+        #pygame.display.flip()
+
+class Macgyver:
+
+    def __init__(self, window, space, directionX, directionY, maccoord, mac, position_mac):
+        self.window = window
+        self.space = space
+        self.directionX = directionX
+        self.directionY = directionY
+        self.maccoord = maccoord
+        self.mac = mac
+        self.position_mac = position_mac
+
+    def mac_display(self):
+        window = self.window
         maccoord = self.maccoord
-
-        mac = pygame.image.load("ressource/MacGyver.png").convert_alpha()
-        mactemp = mac.get_rect()
-        window.blit(space, mactemp)
-        mactemp = mactemp.move(0, 32)
-        window.blit(mac, mactemp)
-        pygame.display.flip()
-
+        space = self.space
+        directionX = self.directionX
+        directionY = self.directionY
+        mac = self.mac
+        position_mac = self.position_mac
+        
+        #window.blit(mac, maccoord)
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_s:
+                    window.blit(space, position_mac)
+                    position_mac = position_mac.move(0, 32)
+                    window.blit(mac, position_mac)
+                    pygame.dsplay.flip()
+                if event/key == K_z:
+                    window.blit(space, position_mac)
+                    position_mac = position_mac.move(0, -32)
+                    window.blit(mac, position_mac)
+                    pygame.dsplay.flip()
+                if event/key == K_d:
+                    window.blit(space, position_mac)
+                    position_mac = position_mac.move(32, 0)
+                    window.blit(mac, position_mac)
+                    pygame.dsplay.flip()
+                if event/key == K_q:
+                    window.blit(space, position_mac)
+                    position_mac = position_mac.move(-32, 0)
+                    window.blit(mac, position_mac)
+                    pygame.dsplay.flip()
+            pygame.display.flip()
         #return position_space, position_wall, disX, disY, disZ
     
     #def graphics(self, position_space, position_wall, disX, disY, disZ):
@@ -351,18 +413,18 @@ def main():
     Xcoord, Ycoord, Zcoord = Calc(position, positiontina, X, Y, Z).coordinates_items()
     maccoord = Calc(position, positiontina, X, Y, Z).coordinates_mac()
     tinacoord = Calc(position, positiontina, X, Y, Z).coordinates_tina()
-    window, space = Pygame(maps, maccoord, tinacoord, Xcoord, Ycoord, Zcoord).mapping()
+    window, space, mac, itemX, itemY, itemZ, itemtina, position_mac = Pygame(maps, tinacoord, Xcoord, Ycoord, Zcoord).mapping()
     #print(Items(maps, space_list).item_coord())
     print("".join(maps))
     while 1:
         #move = Input_user().entry_user()
-        Pygame(maps, maccoord, tinacoord, Xcoord, Ycoord, Zcoord).mac_display(window, space)
-        Pygame(maps, maccoord, tinacoord, Xcoord, Ycoord, Zcoord).item_display(window)
-        #Pygame(maps, maccoord, tinacoord, Xcoord, Ycoord, Zcoord).item_display(window)
+        Pygame(maps, tinacoord, Xcoord, Ycoord, Zcoord).item_display(window, itemX, itemY, itemZ, itemtina)
         move = Input_user().entry_raw()
         position = Characters(maps).mc_position()
         tina = Tina(X, Y, Z, maps).tina_ready()
-        direction = Motion(maps, position, move, tina).direction()
+        directionX, directionY = Motion(maps, position, move, tina).direction()
+        #print(directionX, directionY)
+        Macgyver(window, space, directionX, directionY, maccoord, mac, position_mac).mac_display()
         position = Characters(maps).mc_position()
         #print(position)
         X, Y, Z = Collect(position, X, Y, Z).collect_items()
